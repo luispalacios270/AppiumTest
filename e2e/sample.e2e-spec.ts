1d9a62374cb7f9d63ea3bc5cf36657979ee6c687
Luis palacios <luiseduardo27000@gmail.com>
2018-03-15 08:56:30 -0500

    last modification

--- e2e/sample.e2e-spec.ts
+++ e2e/sample.e2e-spec.ts
@@ -19,66 +19,52 @@ describe("sample scenario", () => {
   });
 
   afterEach(async function() {
-    if (this.currentTest.state !== "failed") {
+    if (this.currentTest.state === "failed") {
       await driver.logScreenshot(this.currentTest.title);
     }
   });
 
   it("Should add an 5 every click", async () => {
     const btnTap = await driver.findElementByAccessibilityId("btnToClick");
+
     const label = await driver.findElementByAccessibilityId("labelToShow");
+
     const textWithoutChange = await label.text();
 
-    await btnTap.click();
+    await btnTap.tap();
     assert.equal(await label.text(), `${textWithoutChange}5`);
-
-    await btnTap.click();
-    assert.equal(await label.text(), `${textWithoutChange}55`);
   });
 
   it("Should hide an element", async () => {
     const btnTap = await driver.findElementByAccessibilityId(
       "btnToClickVanish"
     );
-    await btnTap.click();
-
+    await btnTap.tap();
     assert.isFalse(await btnTap.isDisplayed(), "Item shouldn't be visible");
   });
 
   it("Should work form", async () => {
     const btnNavigate = await driver.findElementByAccessibilityId("btnToForm");
-    await btnNavigate.click();
-
+    await btnNavigate.tap();
     const formPage = await driver.findElementByAccessibilityId("formForSubmit");
     const submitBtn = await driver.findElementByAccessibilityId("btnToSubmit");
     const inputs = await driver.findElementsByAccessibilityId("input");
-
     let succesWasFound = false;
     let successMessage;
-
     try {
       successMessage = await driver.findElementByAccessibilityId("success");
       succesWasFound = true;
     } catch (e) {}
-
     assert.isFalse(
       succesWasFound,
       "The congrats message shouldn't be visible at this moment"
     );
-
     await inputs[0].sendKeys("10000");
-
     await inputs[1].sendKeys("Esta es una descripcion cualquiera");
-
     await inputs[2].sendKeys("15000");
-
     await inputs[3].sendKeys("16000");
-
     await submitBtn.tap();
-
     successMessage = await driver.findElementByAccessibilityId("success");
     assert.isTrue(await successMessage.isDisplayed());
-
-    // await successMessage
   });
 });
